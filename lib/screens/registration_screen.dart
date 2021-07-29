aa9c48e7823d1c7e93e2ea955333d1232b842789
aeladrin <andriy.krupych@gmail.com>
2021-07-29 15:21:34 +0300

    text field styling

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -35,6 +35,8 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
               onChanged: (value) {
                 email = value;
               },
@@ -46,6 +48,8 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              obscureText: true,
               onChanged: (value) {
                 password = value;
               },
